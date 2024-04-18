5c16d4b48c2316b73305f1a0f71ba1355b1225c1
Steven Dickerson <poddocdan@gmail.com>
2024-04-17 18:07:43 -0700

    added template

--- weatherAPP/script.js
+++ weatherAPP/script.js
@@ -1,1 +1,3 @@
-fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`)
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}`) {
+    .then((whateverstuff) => {})
+}
